a1d46502098b14c253918288ff0d356d0e994b7a
Edwin Silvestre <a02305847@aggies.usu.edu>
2024-03-14 16:57:34 -0600

    Cerate cabinet Needs reload/redirect

--- src/app/cabinet/create/page.jsx
+++ src/app/cabinet/create/page.jsx
@@ -1,21 +1,51 @@
 "use client";
-import { useState } from 'react';
+import { useState ,useEffect} from 'react';
+import { supabase } from '@/lib/supabase';
 export default function Page() {
-    const [cabinetName, setCabinetName] = useState('');
-    const [cabinetDescription, setCabinetDescription] = useState('');
-  
-    const handleFormSubmit = (event) => {
-      event.preventDefault();
-      // Placeholder function for handling form submission
-      console.log('Cabinet Name:', cabinetName);
-      console.log('Cabinet Description:', cabinetDescription);
-      // You can add your logic here to submit the form data
-    };
+  // State
+  const [user_id, setUser_id] = useState('');
+  const [cabinetName, setCabinetName] = useState('');
+  const [cabinetDescription, setCabinetDescription] = useState('');
+
+  const supaBaseCabinets = async () => {
+    const { data, error } = await supabase
+      .from('medicinecabinets')
+      .insert([
+        { user_id, name: cabinetName, description: cabinetDescription }
+      ]);
+
+    if (data) {
+      console.log('Cabinet created successfully:', data);
+        location.reload();
+    }
+
+    if (error) {
+      console.error('Error creating cabinet:', error);
+    }
+  };
+
+  const handleFormSubmit = (event) => {
+    event.preventDefault();
+    supaBaseCabinets();
+  };
+
+  useEffect(() => {
+    // Reset form inputs after successful submission
+    setUser_id('');
+    setCabinetName('');
+    setCabinetDescription('');
+  }, []); // Reset inputs when router path changes
+
+
     return( 
     <div className="bg-gray-100 min-h-screen flex flex-col justify-center items-center">
     <div className="max-w-md w-full bg-white p-8 rounded-lg shadow-md">
       <h1 className="text-3xl font-bold text-center mb-6">Create New Cabinet</h1>
       <form onSubmit={handleFormSubmit}>
+        <div className="mb-4">
+          <label htmlFor="user_id" className="block text-gray-700 font-semibold mb-2">user_id</label>
+          <input type="text" id="user_id" value={user_id} onChange={(e) => setUser_id(e.target.value)} className="w-full px-3 py-2 border border-gray-300 rounded focus:outline-none focus:border-blue-500" />
+        </div>
         <div className="mb-4">
           <label htmlFor="cabinetName" className="block text-gray-700 font-semibold mb-2">Name</label>
           <input type="text" id="cabinetName" value={cabinetName} onChange={(e) => setCabinetName(e.target.value)} className="w-full px-3 py-2 border border-gray-300 rounded focus:outline-none focus:border-blue-500" />
