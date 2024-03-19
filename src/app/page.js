84c8abbb807732c7b580120bbfaaa77b832d4e81
Edwin Silvestre <a02305847@aggies.usu.edu>
2024-03-18 20:37:50 -0600

    UI: Buttons

--- src/app/page.js
+++ src/app/page.js
@@ -44,17 +44,23 @@ export default function Home() {
           <h1 className="text-3xl font-bold mb-8">Welcome to Medicine Cab</h1>
           
           {/* Your cabinets */}
-          <div className="text-left mb-8">
-            <h2 className="text-lg font-semibold mb-4">Your cabinets</h2>
-            <Link href="cabinet"><button className="ml-2 mb-3 bg-blue-500 text-white px-6 py-1 rounded">View Cabinet</button></Link>
-            <ul className="space-y-2">
-              {cabinets.map((cabinet) => (
-                <li key={cabinet.cabinet_id}>
-                  <span className="font-semibold">{cabinet.name}</span>
-                  <Link href={`cabinet/${cabinet.cabinet_id}`}><button className="ml-2 bg-blue-500 text-white px-2 py-1 rounded">View Cabinet</button></Link>
-                </li>
-              ))}
-            </ul>
+          <div className="flex justify-center">
+            <div className="text-left mb-8">
+                <h2 className="text-lg font-semibold mb-4">Your cabinets</h2>
+                <Link href="cabinet">
+                    <button className="ml-2 mb-3 bg-blue-500 text-white px-6 py-1 rounded">View Cabinet</button>
+                </Link>
+                <ul className="space-y-2">
+                    {cabinets.map((cabinet) => (
+                        <li key={cabinet.cabinet_id}>
+                            <span className="font-semibold">{cabinet.name}</span>
+                            <Link href={`cabinet/${cabinet.cabinet_id}`}>
+                                <button className="ml-2 bg-blue-500 text-white px-2 py-1 rounded">View Cabinet</button>
+                            </Link>
+                        </li>
+                    ))}
+                </ul>
+            </div>
           </div>
   
           {/* Expire Soon */}
@@ -62,7 +68,7 @@ export default function Home() {
           <h2 className="text-lg font-semibold mb-4">Expire Soon</h2></div>
           <div className="flex justify-end items-center mb-2">
             <div className="text-right mb-2">
-              <Link href="../medicine"><button className="bg-blue-500 hover:bg-blue-600 text-white font-semibold px-4 py-2 rounded-lg focus:outline-none">View All</button></Link>
+              <Link href="../medicine"><button className="bg-blue-500 hover:bg-blue-600 text-white font-semibold mr-2 px-4 py-2 rounded-lg focus:outline-none">View All</button></Link>
             </div>
             <div className="text-right mb-2">
               <Link href="../medicine/create"><button className="bg-blue-500 hover:bg-blue-600 text-white font-semibold px-4 py-2 rounded-lg focus:outline-none">Add New Medicine</button></Link>
